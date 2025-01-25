5f08d900e1b3a2f53c5eb4e39c1846d47ee88b58
Ayush-raikwar <ayushraikwar45@gmail.com>
2025-01-25 13:22:36 +0530

    fixed date values

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const defaultTimetable = [
-    { id: '1', subject: 'Math', time: '9:00 AM - 10:00 AM', date: 'Monday' },
-    { id: '2', subject: 'Science', time: '10:30 AM - 11:30 AM', date: 'Monday' },
+    { id: '1', subject: 'Math', time: '9:00 AM - 10:00 AM', date: '12-06-2025' },
+    { id: '2', subject: 'Science', time: '10:30 AM - 11:30 AM', date: '14-06-2025' },
 ];
 
 export const constants = {
